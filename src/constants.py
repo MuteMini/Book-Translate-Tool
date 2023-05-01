257fa5f3c899c38d3f25da10fa1f9ab49150488f
Min <63823931+MuteMini@users.noreply.github.com>
2023-05-01 18:42:54 -0400

    Refactored detection algorithm for fixing accuracy issues

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,11 @@
+import numpy as np
+
 ACCEPTABLE_FILES = ['png', 'jpg', 'jpeg']
 ACCEPTABLE_FILE_DIALOG = (''.join([f"*.{x} " for x in ACCEPTABLE_FILES]))[:-1]
-CROP_RATIO = 1.545
 
+# Used in detection.py
+CROP_RATIO = 1.545
+THETA_THRESH = np.pi/45
+RHO_THRESH = 25
+
+LINE_THRESH = 5*THETA_THRESH
--- src/detection.py
+++ src/detection.py
@@ -1,81 +1,92 @@
 import constants
 
-import cv2
-import colorsys
-import imutils
-import numpy as np
-
+import cv2, colorsys, imutils, numpy as np
 from PIL import Image
 
 import keras_ocr.pipeline
 
-THETA_THRESH = np.pi/45
-RHO_THRESH = 25
+class Line(object):
+    def __init__(self, rho=None, t=None):
+        self.rho = rho
+        self.t = t
+        self.sin = self.cos = None
+        if t is not None:
+            self.sin = np.sin(t)
+            self.cos = np.cos(t)
 
-class DocumentLines(object):
-    class Line(object):
-        def __init__(self, rho=None, t=None):
-            self.rho = rho
-            self.t = t
-            self.sin = self.cos = None
-            if t is not None:
-                self.cos = np.cos(t)
-                self.sin = np.sin(t)
+    def __gt__(self, other):
+        return self.rho > other.rho
 
-        def dot_product(self, other) -> float:
-            return np.arccos(np.clip(self.cos*other.cos + self.sin*other.sin, -1, 1))
-
-        def get_intersection(self, other) -> tuple[int, int]:
-            d = self.cos*other.sin - other.cos*self.sin
-            if d != 0:
-                x = (self.rho*other.sin - other.rho*self.sin)/d
-                y = (self.cos*other.rho - other.cos*self.rho)/d
-                return (int(x), int(y))
+    def dot_product(self, other) -> float:
+        return np.arccos(np.clip(self.cos*other.cos + self.sin*other.sin, -1, 1))
+    
+    def intersection(self, other) -> tuple[int, int]:
+        d = self.cos*other.sin - other.cos*self.sin
+        if d == 0:
             return None
-
-        def get_list(self) -> list:
-            return [self.rho, self.t]
-
-        def get_trig(self) -> list:
-            return [self.cos, self.sin]
-
-    # Even indices represents min rho line near similar theta lines. Odd represents max rho.
-    def __init__(self, shape):
+        x = (self.rho*other.sin - other.rho*self.sin)/d
+        y = (self.cos*other.rho - other.cos*self.rho)/d
+        return (int(x), int(y))
+    
+    def list(self):
+        return [self.rho, self.t]
+    
+    def trig(self):
+        return [self.cos, self.sin]
+    
+class Document(object):
+    # i and i+1 index represents min rho line near similar theta lines.
+    def __init__(self, shape: tuple[int,int]):
         self.width, self.height = shape
         self.lines = [None for _ in range(4)]
 
-    # Only stores the minimum and maximum distance pairs of lines.
     def add_line(self, rho, theta):
-        line = DocumentLines.Line(rho, theta)
+        line = Line(rho, theta)
 
-        for id in range(0, len(self.lines), 2):
-            if self.lines[id] is None:
-                self.lines[id] = line
+        for i in range(0, len(self.lines), 2):
+            if self.lines[i] is None:
+                self.lines[i] = line
                 break
-            elif self.lines[id].dot_product(line) <= 5*THETA_THRESH:
-                if self.lines[id + 1] is None:
-                    self.lines[id + 1] = line
-                    if self.lines[id].rho > self.lines[id + 1].rho:
-                        self.lines[id], self.lines[id + 1] = self.lines[id+1], self.lines[id]
-                elif self.lines[id].rho > rho:
-                    self.lines[id] = line
-                elif rho > self.lines[id + 1].rho:
-                    self.lines[id + 1] = line
-                break
+            
+            if self.lines[i].dot_product(line) > constants.LINE_THRESH:
+                continue
 
-    def document_found(self) -> bool:
+            if self.lines[i+1] is None:
+                if self.lines[i] > line:
+                    self.lines[i+1] = self.lines[i]
+                    self.lines[i] = line
+                else:
+                    self.lines[i+1] = line
+            
+            self.lines[i] = line if self.lines[i] > line else self.lines[i]
+            self.lines[i+1] = line if line > self.lines[i+1] else self.lines[i+1] 
+            break
+
+    def document_found(self):
         return self.lines[1] is not None and self.lines[3] is not None
+    
+    def lines(self):
+        return [line.list() for line in self.lines if isinstance(line, Line)]
 
-    def get_lines(self) -> list:
-        return [line.get_list() for line in self.lines if isinstance(line, DocumentLines.Line)]
+    def trigs(self):
+        return [line.trig() for line in self.lines if isinstance(line, Line)]
 
-    def get_trigs(self) -> list:
-        return [line.get_trig() for line in self.lines if isinstance(line, DocumentLines.Line)]
+    def corners(self) -> np.ndarray:
+        points = []
 
-    # Temporary testing function
+        for i in range(4): 
+            p = self.lines[i//2].get_intersection(self.lines[i%2 + 2])
+
+            if p is None or (0, 0) > p > (self.width, self.height):
+                return None
+            points.append(p)
+
+        return points
+
+    # Temporary testing function to be replaced with drawing on pyQT objects
     def draw_lines(self, img):
         for n, line in enumerate(self.lines):
-            if isinstance(line, DocumentLines.Line):
+            if isinstance(line, Line):
                 x0 = line.cos*line.rho
                 y0 = line.sin*line.rho
                 x1 = int(x0 + 1000*(-line.sin))
@@ -84,31 +95,6 @@ class DocumentLines(object):
                 y2 = int(y0 - 1000*(line.cos))
                 r, g, b = colorsys.hsv_to_rgb(n*0.1, 0.8, 1)
                 cv2.line(img, (x1, y1), (x2, y2), (b*225, g*225, r*225), 2)
-
-    # As a document, we should have four corners of the document to manipulate.
-    def get_corners(self) -> np.ndarray:
-        points = []
-
-        # Compares 0w/2, 0w/3, 1w/2, 1w/3
-        for id in range(len(self.lines)):
-            p = self.lines[id//2].get_intersection(self.lines[id%2 + 2])
-            if p is None:
-                return None
-            elif (0, 0) > p > (self.width, self.height):
-                return None
-            points.append(p)
-
-        return np.array(points)
-    
-    # Gives how rectangular/most likely it is to be a document in a [0,100] scale.
-    def rectness(self) -> int:
-        pass
-
-    def __str__(self):
-        out = ""
-        for id in range(len(self.lines)-1):
-            out += f"{self.lines[id].get_list()}, "
-        return out + f"{self.lines[-1].get_list()}"
 
 class DocUtils:
     # Comes from https://pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example
@@ -120,8 +106,8 @@ class DocUtils:
         s = np.sum(pts, axis=1)
         rect[0] = pts[np.argmin(s)]
         rect[2] = pts[np.argmax(s)]
-        # tr, bl has most difference in points
 
+        # tr, bl has most difference in points
         d = np.diff(pts, axis=1)
         rect[1] = pts[np.argmin(d)]
         rect[3] = pts[np.argmax(d)]
@@ -138,13 +124,8 @@ class DocUtils:
         width_b = np.sqrt(((tr[0] - tl[0])**2) + ((tr[1] - tl[1])**2))
         max_width = max(int(width_a), int(width_b))
 
-        # for letter paper ratio, w/h = 1.294
+        # Transforms to letter paper ratio, w/h = 1.294
         max_height = int(max_width*constants.CROP_RATIO)
-
-        # height_a = np.sqrt(((tr[0] - br[0])**2) + ((tr[1] - br[1])**2))
-        # height_b = np.sqrt(((tl[0] - bl[0])**2) + ((tl[1] - bl[1])**2))
-        # max_height = max(int(height_a), int(height_b))
-        # max_height = max(int(max_width*1.294), max_height)
 
         dst = np.array([
             [0, 0],
@@ -157,73 +138,78 @@ class DocUtils:
 
     def midpoint(a, b):
         return (int((a[0] + b[0])/2), int((a[1] + b[1])/2))
+    
+    # Returns original image and corners of detected document
+    def find_document(path):
+        original = cv2.imread(path)
+        ratio = original.shape[0] / 600
 
-    # Returns original opencv image and the corners of the detected document.
-    def get_document(path):
-        org_img = cv2.imread(path)
-        ratio = org_img.shape[0]/600.0
-        img = imutils.convenience.resize(org_img.copy(), height=600)
+        image = imutils.convenience.resize(original.copy(), height=600)
 
-        # Processes Image for Document Detection
-        kernel = np.ones((7, 7), np.uint8)
-        edges = cv2.morphologyEx(img.copy(), cv2.MORPH_CLOSE, kernel, iterations=3)
-        frame = cv2.GaussianBlur(edges, (7, 7), 5)
+        # Image processing for HoughLine
+        kernel = np.ones((7,7), np.uint8)
+        edges = cv2.morphologyEx(image.copy(), cv2.MORPH_CLOSE, kernel, iterations=3)
+        cv2.imshow("test", edges)
+        cv2.waitKey(0)
+        frame = cv2.GaussianBlur(edges, (7,7), 5)
         edges = cv2.addWeighted(edges, 2, frame, -1, 0)
+        cv2.imshow("test", edges)
+        cv2.waitKey(0)
         edges = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
         edges = cv2.GaussianBlur(edges, (11, 11), 0)
         edges = cv2.Canny(edges, 0, 100, apertureSize=3)
+        cv2.imshow("test", edges)
+        cv2.waitKey(0)
 
-        # Processing HoughLines into the four most likely document items
-        strong_lines = DocumentLines(img.shape[:2])
-        all_lines = DocumentLines(img.shape[:2])
+        # Processing HoughLines to find most likely document lines
+        strong_lines = Document(image.shape[:2])
+
         lines = cv2.HoughLines(edges, 1, np.pi/180, 60)
         if lines is not None:
-            # Parses HoughLine space into a list of positive rho lines
+            # Turns all lines into positive rho
             lines = lines[:, 0].tolist()
             lines = [x if x[0] >= 0 else [-x[0], x[1]-np.pi] for x in lines]
 
-            candid_lines = np.array([lines[0]])
+            candids = np.array([lines[0]])
             strong_lines.add_line(lines[0][0], lines[0][1])
-            all_lines.add_line(lines[0][0], lines[0][1])
 
             for line in lines[1:]:
                 rho, theta = line[0], line[1]
-
-                closeness_rho = np.isclose(rho, candid_lines[:,0], atol=RHO_THRESH)
-                closeness_theta = np.isclose(theta, candid_lines[:,1], atol=THETA_THRESH)
+                
+                closeness_rho = np.isclose(rho, candids[:,0], atol=constants.RHO_THRESH)
+                closeness_theta = np.isclose(theta, candids[:,1], atol=constants.THETA_THRESH)
 
                 isclose = np.all([closeness_rho, closeness_theta], 0)
 
                 if not any(isclose):
-                    candid_lines = np.concatenate((candid_lines, [line]))
-                    if not strong_lines.document_found():
-                        strong_lines.add_line(rho, theta)
-                    all_lines.add_line(rho, theta)
+                    candids = np.concatenate((candids, [line]))
+                    strong_lines.add_line(rho, theta)
 
-        # Instead of just using the full houghline space, we could compare the document the moment when its immediately found
-        # v.s. the one found at the end and compare it's "documentness". This may result in a better crop of the document.
+                if strong_lines.document_found():
+                    break
 
-        if not all_lines.document_found() or all_lines.get_corners() is None:
-            return org_img, np.array([(0,0), (org_img.shape[1], 0), (0, org_img.shape[0]), (org_img.shape[1], org_img.shape[0])])
+        corners = strong_lines.corners()
+        if corners is None or not strong_lines.document_found():
+            corners = np.array([(0,0), (original.shape[1], 0), (0, original.shape[0]), (original.shape[1], original.shape[0])])
 
-        corners = all_lines.get_corners() # if (strong_lines.rectness() < all_lines.rectness()) else strong_lines.get_corners()
-        return org_img, corners*ratio
+        return original, corners
+    
+    def text_mask(image, pipeline: keras_ocr.pipeline.Pipeline):
+        ratio = image.shape[0] / 1200
 
-    # Returns the mask to inpaint on the cropped image.
-    def get_text_mask(img, pipeline: keras_ocr.pipeline.Pipeline):
-        r = img.shape[0]/1200
-        pred_img = imutils.convenience.resize(img.copy(), height=1200)
-        prediction_data = pipeline.recognize([pred_img])
+        prediction_image = imutils.convenience.resize(image.copy(), height=1200)
+        prediction_data = pipeline.recognize([prediction_image])
 
-        mask = np.zeros(img.shape[:2], dtype='uint8')
+        mask = np.zeroes(image.shape[:2], dtype='uint8')
         for box in prediction_data[0]:
-            pos = [(box[1][i][0]*r, box[1][i][1]*r) for i in range(4)]
+            bounds = box[1]
+            pos = [(bounds[i][0]*ratio, bounds[i][1]*ratio) for i in range(4)]
             thickness = int(np.sqrt((pos[2][0] - pos[1][0])**2 + (pos[2][1] - pos[1][1])**2))
             cv2.line(mask, DocUtils.midpoint(pos[1], pos[2]), DocUtils.midpoint(pos[0], pos[3]), 255, thickness)
         return mask
-
-    def get_resized_final(img, mask, height=None, width=None):
-        resized = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS) if mask is not None else img
+    
+    def resized_final(image, mask, height=None, width=None):
+        resized = cv2.inpaint(image, mask, 7, cv2.INPAINT_NS) if mask is not None else img
 
         if height is None:
             resized = imutils.convenience.resize(resized, width=width)
@@ -231,9 +217,9 @@ class DocUtils:
             resized = imutils.convenience.resize(resized, height=height)
         return resized
     
-    def opencv_to_pil(img):
-        return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
+    def opencv_to_pil(image):
+        return Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    
 if __name__ == '__main__':
     path = "C:\\Users\\there\\Desktop\\New folder\\IMG_0464.jpg"
     doc, corners = DocUtils.get_document(path)
