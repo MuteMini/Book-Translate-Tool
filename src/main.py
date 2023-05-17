c05af9aa02b147a6bcff0e04694472a4fb95f857
Min <63823931+MuteMini@users.noreply.github.com>
2023-05-16 23:38:24 -0400

    Added app icon

--- src/main.py
+++ src/main.py
@@ -6,7 +6,13 @@ import os
 import qdarktheme
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtGui import ( QGuiApplication, QIcon )
+
+# Workaround to show icon on windows taskbar.
+# Took from https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
+import ctypes
+myappid = 'mutemini.booktranslatorapp'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 os.environ['MEMORY_ALLOCATED'] = '0.1'
@@ -22,6 +28,8 @@ if __name__ == '__main__':
 
     window = MainWindow()
     window.resize(max_size)
+    window.setWindowTitle('Book Wiper')
+    window.setWindowIcon(QIcon('icon.png'))
     window.show()
 
     sys.exit(app.exec())
